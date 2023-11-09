22b9b619d3bee0b31499efcbe018e42d56a86c22
Shohruz Nuraddinov <92042320+shohruzcoder@users.noreply.github.com>
2023-11-09 11:16:06 +0500

    commit

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Name(models.Model):
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
