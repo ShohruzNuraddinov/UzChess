15e26fd9f97307d5b5a1e4aed02d51adb186c9bf
eraliabd <eraliabdinazarov22@gmail.com>
2023-11-09 16:33:50 +0500

    feat: rest settings

--- core/settings.py
+++ core/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_yasg',
     'ckeditor',
-    
+    'ckeditor_uploader',
+
     # custom-apps
     'user',
     'news',
@@ -137,6 +138,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
