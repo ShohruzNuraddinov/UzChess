d96c4c5f83012bf98cc2a7e7706f70c41e32d92a
eraliabd <eraliabdinazarov22@gmail.com>
2023-11-09 13:27:17 +0500

    feat: add book models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
+
+from utils.models import BaseModel
+
+
+class DegreeChoice(models.TextChoices):
+    amateur = "Havaskor"
+    initial = "Boshlang'ich"
+    professional = "Professional"
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.full_name
+
+
+class BookCategory(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(BaseModel, DegreeChoice):
+    # relations
+    category = models.ForeignKey(BookCategory, on_delete=models.CASCADE, related_name='books')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author_books')
+
+    # choice fields
+    degree = models.CharField(max_length=20, choices=DegreeChoice.choices, default=DegreeChoice.initial)
+
+    # fields
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='book_images/')
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    old_price = models.DecimalField(max_digits=20, decimal_places=2)
+    number_of_pages = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.author} - {self.title}"
