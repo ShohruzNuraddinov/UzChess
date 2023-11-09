0907dc46fffd9496e1a9043b706b540ef1fbc36a
eraliabd <eraliabdinazarov22@gmail.com>
2023-11-09 16:52:40 +0500

    feat: add jwt

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,30 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="UzChess",
+        default_version='v1',
+        description="API for UzChess",
+        contact=openapi.Contact(email="eralideveloper@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('api/v1/', include('user.urls'))
 ]
