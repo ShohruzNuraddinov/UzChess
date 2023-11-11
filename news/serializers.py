f45ba58b3b71254ec993147cb13f47b6f88aee72
eraliabd <eraliabdinazarov22@gmail.com>
2023-11-11 11:43:27 +0500

    feat: api

--- news/serializers.py
+++ news/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
 
+from .models import Tag, New
 
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'title']
+
+
+class NewSerializer(serializers.ModelSerializer):
+    # view = serializers.IntegerField(source='hit_count.hits', read_only=True)
+
+    class Meta:
+        model = New
+        fields = ['id', 'title', 'image', 'content', 'view', 'tag', 'created_at']
--- news/views.py
+++ news/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.response import Response
+from rest_framework.parsers import FormParser, MultiPartParser
 
-# Create your views here.
+from django.db.models import Q
+from hitcount.models import HitCount
+
+from .models import New, Tag
+from .serializers import NewSerializer, TagSerializer
+
+
+class TagListAPIView(ListCreateAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class NewsListAPIView(ListCreateAPIView):
+    queryset = New.objects.all().order_by('-created_at')
+    serializer_class = NewSerializer
+    parser_classes = [FormParser, MultiPartParser]
+
+
+class NewsDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = New.objects.all()
+    serializer_class = NewSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        if request.user.is_authenticated:
+            New.objects.update_or_create(news=instance, user=request.user)
+
+        return Response(serializer.data)
+
+
+class NewQueryListAPIView(ListAPIView):
+    serializer_class = NewSerializer
+
+    def get_queryset(self):
+        search_query = self.request.query_params.get('search')
+
+        if search_query:
+            return New.objects.filter(
+                Q(title__icontains=search_query) | Q(content__icontains=search_query)
+            )
+        return New.objects.all().order_by('-created_at')
